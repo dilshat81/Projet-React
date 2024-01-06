e5ef555d59f27857e32c53137d25a367f77fb070
dilshat81 <dilshat917@gmail.com>
2024-01-06 23:46:50 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import  Navigation  from '../components/Navigation';
+import  Logo  from '../components/Logo';
+
+const componentName = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A Propos</h1>
+            <br/>
+            <p>lorem ipsun 100 </p>
+            <br/>
+            <p>lorem ipsun 100 </p>
+        </div>
+    );
+};
+
+export default componentName;
